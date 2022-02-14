ffd1300f930067f19f54d7a3d69f0601727bb078
Gurrex <ptkuz@yandex.ru>
2022-02-14 21:01:45 +0700

    Доделал немного

--- WpfApp2/obj/Debug/net6.0-windows/App.g.cs
+++ WpfApp2/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace WpfApp2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WpfApp2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             WpfApp2.App app = new WpfApp2.App();
             app.InitializeComponent();
